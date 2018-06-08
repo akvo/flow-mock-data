c92999a6a7ee2d0306a7c6ce8c7a50a05ab4d4fa
valeria <valerie.cardoso@gmail.com>
2018-06-08 17:14:57 +0200

    creating a mock image

--- app/src/main/java/org/akvo/mockcaddisfly/MainActivity.java
+++ app/src/main/java/org/akvo/mockcaddisfly/MainActivity.java
@@ -2,7 +2,11 @@ package org.akvo.mockcaddisfly;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -14,6 +18,8 @@ import org.akvo.mockcaddisfly.data.ResultBuilder;
 import org.akvo.mockcaddisfly.data.Test;
 import org.akvo.mockcaddisfly.data.result.TestResult;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -53,11 +59,51 @@ public class MainActivity extends AppCompatActivity {
         if (!TextUtils.isEmpty(mockTestResult)) {
             Intent resultIntent = new Intent(getIntent());
             resultIntent.putExtra("response", mockTestResult);
+            String getImageFilePath = getImagePathExtra();
+            resultIntent.putExtra("image", getImageFilePath);
             setResult(Activity.RESULT_OK, resultIntent);
         } else {
             setResult(RESULT_CANCELED);
         }
         finish();
+    }
+
+    @NonNull
+    private String getImagePathExtra() {
+        File file = saveImageToFile();
+        return file.getAbsolutePath();
+    }
+
+    private File saveImageToFile() {
+        File file = createImageFile();
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.caddisfly_img);
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(file);
+            icon.compress(Bitmap.CompressFormat.PNG, 100, out);
+        } catch (Exception e) {
+            Timber.e(e);
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                Timber.e(e);
+            }
+        }
+        return file;
+    }
+
+    @NonNull
+    private File createImageFile() {
+        File root = Environment.getExternalStorageDirectory();
+        File dir = new File(
+                root.getAbsolutePath() + File.separator + "Akvo Caddisfly" + File.separator + "result-images");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        return new File(dir, "cad_123.png");
     }
 
     public String getMockTestResult(@Nullable String testTypeUuid) {
--- app/src/main/java/org/akvo/mockcaddisfly/data/RandomTestResultGenerator.java
+++ app/src/main/java/org/akvo/mockcaddisfly/data/RandomTestResultGenerator.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 
 import java.util.Random;
 
+import timber.log.Timber;
+
 public class RandomTestResultGenerator {
 
     public static final Double DEFAULT_VALUE = 0.0;
@@ -14,6 +16,7 @@ public class RandomTestResultGenerator {
 
     @NonNull
     public Double getResultValue(String ranges) {
+        Timber.i("ranges "+ranges);
         if (TextUtils.isEmpty(ranges)) {
             return DEFAULT_VALUE;
         } else {
@@ -23,8 +26,9 @@ public class RandomTestResultGenerator {
             } else if (split.length == 1) {
                 return Double.parseDouble(split[0]);
             } else {
-                double max = Double.parseDouble(split[1]);
-                double randomDouble = new Random().nextInt((int) max);
+                double max = Double.parseDouble(split[split.length - 1]);
+                int bound = (int) Math.max(max, 1); //cannot be less than 1
+                double randomDouble = new Random().nextInt(bound);
                 double min = Double.parseDouble(split[0]);
                 randomDouble = Math.min(Math.max(randomDouble, min), max);
                 return randomDouble;
