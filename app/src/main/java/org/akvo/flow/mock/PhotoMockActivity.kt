e8beabf5931d9cd676885430d18633bacc2cc05e
valeria <valerie.cardoso@gmail.com>
2018-08-07 12:18:51 +0200

    Added way to mock images

---
+++ app/src/main/java/org/akvo/flow/mock/PhotoMockActivity.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2018 Stichting Akvo (Akvo Foundation)
+ *
+ * This file is part of Akvo Flow.
+ *
+ * Akvo Flow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Akvo Flow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Akvo Flow.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.akvo.flow.mock
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.akvo.flow.mock.util.FileUtils
+import timber.log.Timber
+import java.io.File
+import java.io.FileOutputStream
+
+class PhotoMockActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mock_caddisfly)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val imagePath : Uri? = intent.getParcelableExtra(android.provider.MediaStore.EXTRA_OUTPUT)
+        val resultIntent = Intent(intent)
+        if (imagePath != null) {
+            copyImage(imagePath)
+            setResult(Activity.RESULT_OK, resultIntent)
+        } else {
+            setResult(Activity.RESULT_CANCELED)
+        }
+        finish()
+    }
+
+    private fun copyImage(imagePath: Uri) {
+        val icon = BitmapFactory.decodeResource(resources, R.drawable.akvo_image)
+        var out: FileOutputStream? = null
+        try {
+            out = FileOutputStream(File(imagePath.path))
+            icon.compress(Bitmap.CompressFormat.PNG, 100, out)
+        } catch (e: Exception) {
+            Timber.e(e)
+        } finally {
+            out?.let { FileUtils.close(it) };
+        }
+    }
+
+}
